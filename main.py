987de5a24948b8dcf33e22a334c7bc929cdfdb4f
Marenthe de Jong <110374@st-maartenscollege.nl>
2020-06-18 07:00:20 +0000

    De lijst van woorden toegevoegd waaruit de computer straks kan gaan kiezen.

---
+++ main.py
@@ -0,0 +1,5 @@
+
+
+words= ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier','fotografie', 'waardebepaling', 'specialiteit', 'verzekering','universiteit','heesterperk']
+
+#alle woorden waar de computer uit kan kiezen
