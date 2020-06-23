93aa1f8a1cd7a9abeee046ccb30165755912249d
Marenthe de Jong <110374@st-maartenscollege.nl>
2020-06-23 11:39:05 +0000

    Code toegevoegd die een woord uit de lijst random moet kiezen.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-
+import random
+#zorgt dat er een random woord wordt gekozen uit de lijst
 
 words= ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier','fotografie', 'waardebepaling', 'specialiteit', 'verzekering','universiteit','heesterperk']
-
 #alle woorden waar de computer uit kan kiezen
+
+
+
