b6e4639f4d87cc1fa8c286b492c5c7ede2849d82
Marenthe de Jong <110374@st-maartenscollege.nl>
2020-06-29 11:56:56 +0000

    for loopje toegevoegd om aantal letters weer te geven met '-' en letter op het juiste plekje in het woord neer te zetten

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 words= ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier','fotografie', 'waardebepaling', 'specialiteit', 'verzekering','universiteit','heesterperk']
 #alle woorden waar de computer uit kan kiezen
 
-
 word = random.choice(words)  
 #definieert wat de variabele 'word' is
 
@@ -14,5 +13,17 @@ print('Je hebt 5 beurten! Het woord is', len(word), 'letters lang')
 guess = input ('Raad een letter! Of raad het woord als je het al weet!')
 #vraagt om input speler 
 
+guesses =''
+
 turns = 5 
 #zorgt dat er max. 5 beurten zijn
+
+for letter in word:  
+          
+  if letter in guesses:  
+    print(letter) 
+             
+  else:  
+     print("_")
+#laat aantal letters zien en de goed geraden letter op de juiste plek
+
