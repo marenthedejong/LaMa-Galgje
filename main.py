4de5aa6c11bfbbf69218125369c82c58770bfac3
Marenthe de Jong <110374@st-maartenscollege.nl>
2020-07-01 11:10:17 +0000

    Het hele spel in een function gezet en een welkomsboodschap toegevoegd. Helemaal onderaan de naam van de function gezet om het spel te laten beginnen.

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-import random
-#zorgt dat er een random woord wordt gekozen uit de lijst
+def galgje():
 
-words= ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier','fotografie', 'waardebepaling', 'specialiteit', 'verzekering','universiteit','heesterperk']
-#alle woorden waar de computer uit kan kiezen
+ import random
+ #zorgt dat er een random woord wordt gekozen uit de lijst
 
-word = random.choice(words)  
-#definieert wat de variabele 'word' is
+ words= ['informatica', 'informatiekunde', 'spelletje', 'aardigheidje', 'scholier','fotografie', 'waardebepaling', 'specialiteit', 'verzekering','universiteit','heesterperk']
+ #alle woorden waar de computer uit kan kiezen
 
-print('Je hebt 5 beurten! Het woord is', len(word), 'letters lang')
-#geeft weer hoe lang het woord is
+ word = random.choice(words)  
+ #definieert wat de variabele 'word' is en laat computer random woord kiezen
 
-guesses =''
+ print('Welkom bij lama galgje!')
 
-turns = 5 
-#zorgt dat er max. 5 beurten zijn
+ print('Je hebt 5 beurten! Het woord is', + len(word), 'letters lang')
+ #geeft weer hoe lang het woord is
 
-while turns > 0: 
+ guesses = ''
+
+ turns = 5 
+  #zorgt dat er max. 5 beurten zijn
+
+ while turns > 0: 
      #wat er gebeurt als er nog beurten zijn 
      
     failed = 0 
     #aantal keer dat er fouten worden gemaakt
     
     for letter in word:  
-          
         
         if letter in guesses:  
             print(letter) 
@@ -33,37 +36,41 @@ while turns > 0:
             failed +=1
            #het aantal fouten neemt met 1 toe
 
-
     if failed == 0: 
         
         print('Je hebt gewonnen, gefeliciteerd!')  
       
         print("Het woord is: ", word)  
-        break #zorgt ervoor dat de loop stopt als je wint
         #hele stuk is wat er gebeurt als je wint
-
-
-    guess = input ('Raad een letter! Of raad het woord als je  het al weet!')
-    #vraagt om input speler
-
+        
+    guess = input("Raad een letter:")
+    
     guesses += guess  
-    #laat de computer onthouden welke letters al zijn geraden
-      
-  
-    if guess not in word: 
+    #laat de computer onthouden welke letters al zijn geraden 
+
+    if guess not in word:
           
         turns -= 1
          #aantal beurten neemt met 1 af 
        
         print("FOUT") 
-          
-       
         print("Je hebt nog maar", + turns, 'beurten!')
-        #zorgt dat er een beurt af gaat als de geraden letter niet in het woord zit 
+
+        
         if turns == 0: 
             print('Je hebt verloren, jammer joh!') 
             print("Het woord is: ", word)
             #verliesbericht voor als beurten op zijn
+          
+
+galgje()
+#laat het spel beginnen
+
+  
+
+    
+         
+
 
 
     
